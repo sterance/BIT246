d0c8e1cd02b2f3ef63cba6e18e5eb5f8fd8f2da7
Chris Smith <smithc9083@gmail.com>
2026-07-30 09:33:37 +1000

    week 2 lab

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BIT246
+- classwork for HEBSIT BIT246 - Object Oriented RAD by Chris Smith, S1564427
---
+++ Week_02_Lab/evens.py
@@ -0,0 +1,22 @@
+import pandas as pd
+print("Pandas is ready.")
+
+data = pd.read_excel("/home/mp/BIT246/Week_02_Lab/Even_Odd_Numbers.xlsx", sheet_name="Numbers")
+
+def isEven(input_num):
+    return input_num % 2 == 0
+
+evens = set()
+odds = set()
+
+for number in data["Number"]:
+    if pd.notna(number):
+        if isEven(number):
+            evens.add(number)
+            print(f"{number} is even.")
+        else:
+            odds.add(number)
+            print(f"{number} is odd.")
+
+print("The largest even number is", max(evens))
+print("The smallest odd number is", min(odds))
